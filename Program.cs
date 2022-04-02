33baf15ec9347d60a78ae047b281702bff5ff03e
leeool <lgonsalez02@gmail.com>
2022-04-02 13:10:37 -0300

    finaliza o exercicio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿double dist, tempo;
+
+Console.Write("Insira a distância percorrida (m): ");
+dist = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Insira o tempo gasto (s): ");
+tempo = Convert.ToDouble(Console.ReadLine());
+
+double velcmedia = dist / tempo;
+
+Console.WriteLine($"\nA velocidade média é de {velcmedia:N2} m/s");
+
